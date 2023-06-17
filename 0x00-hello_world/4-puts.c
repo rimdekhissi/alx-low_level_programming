8d07872c721ced18b96cb719f08b4fcf5c244151
rimdekhissi <rimdekhissi@outlook.fr>
2023-06-17 09:47:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - A program that print using p
- * Return: 0 (Success)
- */
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
 {
-	puts("\"Programming is like building
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
