733b3d9b35f7acd6f5a581c6451d61ce4e686870
rimdekhissi <rimdekhissi@outlook.fr>
2023-06-21 09:09:01 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_print_to_98.c
+++ 0x02-functions_nested_loops/11-print_print_to_98.c
@@ -15,8 +15,8 @@ for (i = n; i <= 98; i++)
 if (i != 98)
 printf("%d, ", i);
 else if (i == 98)
-{
 printf("%d\n", i);
+}
 }
 else if (n >= 98)
 {
@@ -24,7 +24,6 @@ for (j = n; j >= 98; j--)
 {
 if (j != 98)
 printf("%d, ", j);
-{
 else if (j == 98)
 printf("%d\n", j);
 }
