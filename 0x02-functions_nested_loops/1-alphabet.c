b4d41d1233b82feb3df2c4d52ac54752ca95a18a
rimdekhissi <rimdekhissi@outlook.fr>
2023-06-20 18:32:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,12 @@
  *print_alphabet - a program that print alphabet in lowercase
  *Return: 0
 */
-
 void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
-_putchar (i);}
+{
+_putchar (i);
+}
 _putchar ('\n');
 }
