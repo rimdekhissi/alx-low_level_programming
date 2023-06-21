d96751a705f8949216547ed15c3d3dd83b520fbf
rimdekhissi <rimdekhissi@outlook.fr>
2023-06-21 07:57:48 -0700

    6-abc

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,19 @@
  */
 int print_sign(int n)
 {
-if (n > 0)
+        if (n > 0)
+        {
+                _putchar('+');
+                return (1);
+        }
+       	else if (n == 0)
+}
+                _putchar(48);
+                return (0);
 {
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-}
-_putchar(48);
-return (0);
-}
 else if (n < 0)
-{
-_putchar('-');
-}
-return (-1);
+        {
+                _putchar('-');
+        }
+                return (-1);
 }
